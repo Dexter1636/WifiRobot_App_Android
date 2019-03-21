85247a1ffc852992744b19429815fe67ab54d4e3
Dexter <2779858398@qq.com>
2019-03-21 20:22:39 +0800

    gradle changed

--- app/src/main/java/org/blackant/wifirobot_app_android/FullscreenActivity.java
+++ app/src/main/java/org/blackant/wifirobot_app_android/FullscreenActivity.java
@@ -5,8 +5,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -61,6 +61,7 @@ public class FullscreenActivity extends AppCompatActivity {
             mControlsView.setVisibility(View.VISIBLE);
         }
     };
+    private Button btnSettings;
     private boolean mVisible;
     private final Runnable mHideRunnable = new Runnable() {
         @Override
@@ -73,13 +74,15 @@ public class FullscreenActivity extends AppCompatActivity {
      * system UI. This is to prevent the jarring behavior of controls going away
      * while interacting with activity UI.
      */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+    private final View.OnClickListener jumpToSettingsListener = new View.OnClickListener() {
+        /**
+         * Called when a view has been clicked.
+         *
+         * @param v The view that was clicked.
+         */
         @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
+        public void onClick(View v) {
+            // TODO: 19-3-21 jump to SettingsActivity
         }
     };
 
@@ -92,6 +95,8 @@ public class FullscreenActivity extends AppCompatActivity {
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
+        btnSettings = findViewById(R.id.dummy_button);
+
 
 
         // Set up the user interaction to manually show or hide the system UI.
@@ -105,7 +110,7 @@ public class FullscreenActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        btnSettings.setOnClickListener(jumpToSettingsListener);
     }
 
     @Override
